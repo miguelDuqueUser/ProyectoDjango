ebd1cb26790d011ae1517f000828f384805ff353
miguelDuqueUser <50363944+miguelDuqueUser@users.noreply.github.com>
2019-05-07 15:32:35 +0200

    Update constructor2.sh

--- constructor2.sh
+++ constructor2.sh
@@ -1,7 +1,6 @@
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo apt install python-pip -y
-cd /
 cd /home/$USER/ProyectoDjango
 sudo docker-compose run web django-admin.py startproject composeexample .
 sudo mv /home/$USER/ProyectoDjango/settings.py /home/$USER/ProyectoDjango/composeexample/settings.py
