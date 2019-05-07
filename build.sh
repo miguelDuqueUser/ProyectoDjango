4e30f7ed1a44aa2c449a8cc6c25210a0135125dc
miguelDuqueUser <50363944+miguelDuqueUser@users.noreply.github.com>
2019-05-07 14:16:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,3 @@ sudo apt update
 sudo apt install docker-ce -y
 sudo adduser ubuntu docker
 newgrp docker
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo apt install python-pip -y
-cd /
-cd ProyectoDjango
-sudo docker-compose run web django-admin.py startproject composeexample .
-sudo mv /home/$USER/ProyectoDjango/settings.py /home/$USER/ProyectoDjango/composeexample/settings.py
-cd ProyectoDjango
-sudo chown -R $USER:$USER .
-docker-compose up
