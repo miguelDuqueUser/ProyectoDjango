83c1d99092eb31af1cafd9d0cfff6101c4d6e7d4
miguelDuqueUser <50363944+miguelDuqueUser@users.noreply.github.com>
2019-05-20 15:45:03 +0200

    Delete prueba.sh

--- prueba.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update
-sudo apt install docker.io docker-compose -y
-sudo usermod -aG docker $USER
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo apt install python-pip -y
