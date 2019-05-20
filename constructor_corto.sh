8cfdadddafac0cf2c793e9d1280e6ed9c7a0cb91
miguelDuqueUser <50363944+miguelDuqueUser@users.noreply.github.com>
2019-05-20 14:19:14 +0200

    Update constructor_corto.sh

--- constructor_corto.sh
+++ constructor_corto.sh
@@ -1,6 +1,6 @@
-sudo apt update
+sudo apt-get update
 sudo apt install docker.io docker-compose -y
-sudo adduser ubuntu docker
+sudo usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo apt install python-pip -y
@@ -9,4 +9,4 @@ sudo docker-compose run web django-admin.py startproject composeexample .
 sudo mv /home/$USER/ProyectoDjango/settings.py /home/$USER/ProyectoDjango/composeexample/settings.py
 cd /home/$USER/ProyectoDjango/
 sudo chown -R $USER:$USER .
-sudo docker-compose up -d
+docker-compose up -d
